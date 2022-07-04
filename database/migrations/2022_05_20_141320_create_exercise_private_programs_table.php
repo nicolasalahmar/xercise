0044d33a0a2608f5a8c743dd6501f7cbc319145f
nicolasalahmar <nicolasalahmar@gmail.com>
2022-07-04 17:20:57 +0300

    day_num is 28 days

--- database/migrations/2022_05_20_141320_create_exercise_private_programs_table.php
+++ database/migrations/2022_05_20_141320_create_exercise_private_programs_table.php
@@ -25,7 +25,8 @@ return new class extends Migration
             $table->integer('reps')->nullable();
             $table->integer('sets')->nullable();
             $table->time('duration')->nullable();
-            $table->enum('day_num', ['1','2','3','4','5','6','7']);
+            $table->enum('day_num',['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'
+            ,'18','19','20','21','22','23','24','25','26','27','28']);
         });
     }
 
